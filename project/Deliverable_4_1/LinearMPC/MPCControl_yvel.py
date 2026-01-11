c2b540d55c09888621204c66ef4c3e1989fda6c2
julienmangiatordi <julien.mangiatordi@epfl.ch>
2026-01-11 18:46:19 +0100

    echange de code en vif

--- project/Deliverable_4_1/LinearMPC/MPCControl_yvel.py
+++ project/Deliverable_4_1/LinearMPC/MPCControl_yvel.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 from .MPCControl_base import MPCControl_base
 
 
-class MPCControl_xvel(MPCControl_base):
+class MPCControl_yvel(MPCControl_base):
     x_ids: np.ndarray = np.array([1, 4, 6])
     u_ids: np.ndarray = np.array([1])
 
